c60641796e9366351ef3c9c3952da421bb9f6dd7
m1nce <miinchaankiim@gmail.com>
2024-02-21 06:36:58 -0800

    update?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -685,7 +685,7 @@ p {
 </style>
 
 <div id="container">
-    <img src='/nbacourt.jpg' alt='Basketball Court' width="500" height="500">
+    <img src='static/nbacourt.jpg' alt='Basketball Court' width="500" height="500">
     <svg id="overlay"></svg>
 
     {#if showPlayerInfo}
